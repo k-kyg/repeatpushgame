c3eceb9094f9375e6317f1a3ed45713e98789cef
k-kyg <kygkd38@gmail.com>
2020-01-26 23:12:42 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+if (navigator.userAgent.includes("Mobile")) {
+    document.querySelectorAll(".play-buttons > *").forEach(function (e) {
+        e.classList.add("active");
+    });
+    console.log("it is mobile device");
+}
+else {
+    document.querySelectorAll(".play-buttons > *").forEach(function (e) {
+        e.classList.add("hover");
+    });
+    console.log("it is not mobile device");
+}
+var arrows = document.getElementById("arrows"), dfjk = document.getElementById("dfjk"), space = document.getElementById("space"), enter = document.getElementById("enter"), allkey = document.getElementById("allkey"), click = document.getElementById("click");
+{
+    var arr = [arrows, dfjk, space, enter, allkey, click];
+    arr.forEach(function (e) {
+        var _a;
+        (_a = e) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+            var _a;
+            location.href = "./games/" + ((_a = e) === null || _a === void 0 ? void 0 : _a.id) + ".html";
+        });
+    });
+}
---
+++ ts/main.ts
@@ -0,0 +1,25 @@
+if (navigator.userAgent.includes("Mobile")) {
+	document.querySelectorAll(".play-buttons > *").forEach(e => {
+		e.classList.add("active");
+	});
+	console.log("it is mobile device")
+} else {
+	document.querySelectorAll(".play-buttons > *").forEach(e => {
+		e.classList.add("hover");
+	});
+	console.log("it is not mobile device")
+}
+const arrows: HTMLElement | null = document.getElementById("arrows"),
+	dfjk: HTMLElement | null = document.getElementById("dfjk"),
+	space: HTMLElement | null = document.getElementById("space"),
+	enter: HTMLElement | null = document.getElementById("enter"),
+	allkey: HTMLElement | null = document.getElementById("allkey"),
+	click: HTMLElement | null = document.getElementById("click");
+{
+	const arr: (HTMLElement | null)[] = [arrows, dfjk, space, enter, allkey, click];
+	arr.forEach(e => {
+		e?.addEventListener("click", () => {
+			location.href = `./games/${e?.id}.html`;
+		});
+	});
+}
