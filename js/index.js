261143e9e62c8361667b4983144d810260031208
k-kyg <kygkd38@gmail.com>
2020-01-29 21:55:52 +0900

    ヘルプ画面を追加

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict";
-var _a, _b, _c;
-const arrows = document.getElementById("arrows"), dfjk = document.getElementById("dfjk"), space = document.getElementById("space"), enter = document.getElementById("enter"), allkey = document.getElementById("allkey"), fourkeys = document.getElementById("fourkeys"), click = document.getElementById("click"), settingfield = document.getElementById("settingfield"), setting = document.getElementById("setopt"), closebutton = document.getElementById("closebutton");
+var _a, _b, _c, _d, _e;
+const arrows = document.getElementById("arrows"), dfjk = document.getElementById("dfjk"), space = document.getElementById("space"), enter = document.getElementById("enter"), allkey = document.getElementById("allkey"), fourkeys = document.getElementById("fourkeys"), click = document.getElementById("click"), settingfield = document.getElementById("settingfield"), setting = document.getElementById("setopt"), help = document.getElementById("help"), helpfield = document.getElementById("helpfield"), rankingbtn = document.getElementById("rankbtn"), funcbtns = document.getElementById("funcbtns"), closehelpbtn = document.getElementById("closehelpbtn"), closesettingbtn = document.getElementById("closesettingbtn");
 let option = (_a = document.getElementById("settingfield")) === null || _a === void 0 ? void 0 : _a.querySelectorAll(`input[type="checkbox"]:checked`);
 let optionstr = "";
 {
@@ -20,16 +20,28 @@ let optionstr = "";
 (_b = setting) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
     var _a;
     settingfield.style.zIndex = "2";
-    setting.style.visibility = "hidden";
+    funcbtns.style.visibility = "hidden";
     (_a = settingfield) === null || _a === void 0 ? void 0 : _a.classList.add("active");
 });
-(_c = closebutton) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
+(_c = closesettingbtn) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
     var _a, _b, _c;
     setTimeout(() => settingfield.style.zIndex = "unset", 1000);
-    setTimeout(() => setting.style.visibility = "visible", 1000);
+    setTimeout(() => funcbtns.style.visibility = "visible", 1000);
     (_a = settingfield) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
     option = (_b = document.getElementById("settingfield")) === null || _b === void 0 ? void 0 : _b.querySelectorAll(`input[type="checkbox"]:checked`);
     (_c = option) === null || _c === void 0 ? void 0 : _c.forEach(e => {
         optionstr += `${e.id},`;
     });
+});
+(_d = help) === null || _d === void 0 ? void 0 : _d.addEventListener("click", () => {
+    var _a;
+    helpfield.style.zIndex = "2";
+    funcbtns.style.visibility = "hidden";
+    (_a = helpfield) === null || _a === void 0 ? void 0 : _a.classList.add("active");
+});
+(_e = closehelpbtn) === null || _e === void 0 ? void 0 : _e.addEventListener("click", () => {
+    var _a;
+    setTimeout(() => helpfield.style.zIndex = "unset", 1000);
+    setTimeout(() => funcbtns.style.visibility = "visible", 1000);
+    (_a = helpfield) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
 });
--- ts/index.ts
+++ ts/index.ts
@@ -7,7 +7,12 @@ const arrows: HTMLElement | null = document.getElementById("arrows"),
 	click: HTMLElement | null = document.getElementById("click"),
 	settingfield: HTMLElement | null = document.getElementById("settingfield"),
 	setting: HTMLElement | null = document.getElementById("setopt"),
-	closebutton: HTMLElement | null = document.getElementById("closebutton");
+	help: HTMLElement | null = document.getElementById("help"),
+	helpfield: HTMLElement | null = document.getElementById("helpfield"),
+	rankingbtn: HTMLElement | null = document.getElementById("rankbtn"),
+	funcbtns: HTMLElement | null = document.getElementById("funcbtns"),
+	closehelpbtn: HTMLElement | null = document.getElementById("closehelpbtn"),
+	closesettingbtn: HTMLElement | null = document.getElementById("closesettingbtn");
 let option: NodeListOf<HTMLInputElement> | undefined = document.getElementById("settingfield")?.querySelectorAll(`input[type="checkbox"]:checked`);
 let optionstr: string = "";
 {
@@ -22,15 +27,25 @@ let optionstr: string = "";
 // window.addEventListener("keydown", (event) => console.log(event?.keyCode))
 setting?.addEventListener("click", () => {
 	settingfield!.style.zIndex = "2"
-	setting!.style.visibility = "hidden";
+	funcbtns!.style.visibility = "hidden";
 	settingfield?.classList.add("active");
 });
-closebutton?.addEventListener("click", () => {
+closesettingbtn?.addEventListener("click", () => {
 	setTimeout(() => settingfield!.style.zIndex = "unset", 1000)
-	setTimeout(() => setting!.style.visibility = "visible", 1000);
+	setTimeout(() => funcbtns!.style.visibility = "visible", 1000);
 	settingfield?.classList.remove("active");
 	option = document.getElementById("settingfield")?.querySelectorAll(`input[type="checkbox"]:checked`);
 	option?.forEach(e => {
 		optionstr += `${e.id},`
 	});
+});
+help?.addEventListener("click", () => {
+	helpfield!.style.zIndex = "2"
+	funcbtns!.style.visibility = "hidden";
+	helpfield?.classList.add("active");
+});
+closehelpbtn?.addEventListener("click", () => {
+	setTimeout(() => helpfield!.style.zIndex = "unset", 1000)
+	setTimeout(() => funcbtns!.style.visibility = "visible", 1000);
+	helpfield?.classList.remove("active");
 });
